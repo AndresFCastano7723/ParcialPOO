e79e5be9507b17a36b0093dd060308f7be807011
Gabriel Gomez <gabrielfgomezr05@gmail.com>
2024-03-20 15:59:47 -0500

    Final

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -2,11 +2,16 @@ package principal;
 
 import logica.Calculadora;
 import logica.Hombre;
+import logica.Mujer;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		Calculadora cl = new Hombre(19, 63, 163);
+		System.out.println(cl.TMB());
+		
+		cl = new Mujer(19, 63, 163);
+		System.out.println(cl.TMB());
 	}
 
 }
